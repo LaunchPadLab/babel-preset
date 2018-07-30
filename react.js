6070ede6f91105ce1cbe1b352f0f8b94626408b6
David Pickart <davidstoreypickart@gmail.com>
2018-07-30 11:34:40 -0500

    Modularize redux-form imports

--- react.js
+++ react.js
@@ -3,4 +3,12 @@ module.exports = {
     require('./es6'),
     'react',
   ],
+  'plugins': [
+    ['transform-imports', {
+        'redux-form': {
+          'transform': 'redux-form/es/${member}',
+          'preventFullImport': true
+        }
+    }]
+  ]
 }
